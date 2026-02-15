c9922ba751b8150d07abecb123f4980dfd525741
daniel <ddelga12@charlotte.edu>
2026-02-15 00:55:26 -0500

    added login screen

--- client/src/routes/approutes.jsx
+++ client/src/routes/approutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import ResultsPage from '../pages/resultspage.jsx'
+import Login from "../pages/login.jsx";
 
 export default function AppRoutes () {
   return (
     <Routes>
-      <Route path='/results' element={<ResultsPage />} />
+      <Route path="/results" element={<ResultsPage />} />
+      <Route path="/login" element={<Login />} /> 
     </Routes>
-  )
+  );
 }
