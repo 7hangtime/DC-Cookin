8d489430b936ddda9125dc7c4d864baf02b230ec
daniel <ddelga12@charlotte.edu>
2026-02-20 05:27:51 -0500

    added endpoint to react pantry page to retrieve pantry items and added cors to server.js to allow the endpoint to be called by client

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import pantryRouter from "./routes/pantry.js";
+import cors from "cors";
 
 const app = express()
 const PORT = process.env.PORT || 3000
 const environment = process.env.NODE_ENV
 
+app.use(cors());
 app.use("/api/pantry", pantryRouter);
 app.use(express.json())
 
