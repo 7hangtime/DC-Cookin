06d48c9d218bfb547c93e6607aac89fee1779e11
7hangtime <dthrowe1@charlotte.edu>
2026-02-22 19:22:07 -0500

    Merged pantry-add and new recipe-details

--- client/src/routes/approutes.jsx
+++ client/src/routes/approutes.jsx
@@ -3,14 +3,18 @@ import ResultsPage from '../pages/resultspage.jsx'
 import Login from "../pages/login.jsx";
 import Register from "../pages/register.jsx";
 import Pantry from "../pages/pantry.jsx";
+import PantryAdd from "../pages/pantryadd.jsx";
+import RecipeDetails from "../pages/recipedetails.jsx";
 
-export default function AppRoutes () {
+export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/results" element={<ResultsPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/pantry" element={<Pantry />} />
+      <Route path="/pantry-add" element={<PantryAdd />} />
+      <Route path="/recipe/:id" element={<RecipeDetails />} />
     </Routes>
   );
 }
