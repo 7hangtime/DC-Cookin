8e2a94a49bc92c7a95f28d29cbf907fbf39b4632
7hangtime <dthrowe1@charlotte.edu>
2026-02-22 19:30:06 -0500

    more merge fixes

--- client/src/routes/approutes.jsx
+++ client/src/routes/approutes.jsx
@@ -4,10 +4,7 @@ import Login from "../pages/login.jsx";
 import Register from "../pages/register.jsx";
 import Pantry from "../pages/pantry.jsx";
 import PantryAdd from "../pages/pantryadd.jsx";
-<<<<<<< HEAD
 import RecipeDetails from "../pages/recipedetails.jsx";
-=======
->>>>>>> 862bb52af9ae40120c19594fba514ee2cd5780d8
 
 export default function AppRoutes() {
   return (
@@ -17,10 +14,7 @@ export default function AppRoutes() {
       <Route path="/register" element={<Register />} />
       <Route path="/pantry" element={<Pantry />} />
       <Route path="/pantry-add" element={<PantryAdd />} />
-<<<<<<< HEAD
       <Route path="/recipe/:id" element={<RecipeDetails />} />
-=======
->>>>>>> 862bb52af9ae40120c19594fba514ee2cd5780d8
     </Routes>
   );
 }
