2a911a7d847823520b776d77681cb408e9e76492
7hangtime <161758674+7hangtime@users.noreply.github.com>
2026-02-23 16:53:12 -0500

    Delete .history/client/src/pages directory

--- .history/client/src/pages/pantry_20260223023125.jsx
+++
@@ -1,7 +0,0 @@
-import { useState, useEffect } from "react";
-import { supabase } from "../../supabase";
-
-export default function Pantry() {
-  const [user, setUser] = useState(null);
-  const [pantryItems, setPantryItems] = useState([]);
-  const [newIngredient, set.history/client/src/pages/pantry_20260223003435.jsx
