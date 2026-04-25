2e0919c542009ee1823dc9502ab582e90f04dc5b
7hangtime <dthrowe1@charlotte.edu>
2026-04-25 05:37:29 -0400

    hotfix

--- server/src/services/savedRecipesService.js
+++ server/src/services/savedRecipesService.js
@@ -1,7 +1,7 @@
 import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
-import { supabase } from "../config/supabase.js";
+import { supabase } from "../../supabase.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
